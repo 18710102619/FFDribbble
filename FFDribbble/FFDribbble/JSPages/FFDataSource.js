a5efe7c1aa31d7aa27388b2b4656f559d2174194
张玲玉 <2949820345@qq.com>
2016-08-29 17:54:12 +0800

    请求数据修改

--- FFDribbble/FFDribbble/JSPages/FFDataSource.js
+++ FFDribbble/FFDribbble/JSPages/FFDataSource.js
@@ -6,17 +6,17 @@
   Copyright © 2016年 bj.zly.com. All rights reserved.
 */
 
+require('AFHTTPRequestOperationManager')
+
 var _dataSourceShareInstance;
 
 defineJSClass('FFDataSource', {
-              
     init: function() {
       this.dribbbleHost = 'https://api.dribbble.com/v1';
-      this.requestManager = require('AFHTTPRequestOperationManager').manager();
+      this.requestManager = AFHTTPRequestOperationManager.manager();
       this.requestManager.requestSerializer().setValue_forHTTPHeaderField('Bearer deeb37c0823d3866650db12df9e36730a0453a5a7b8e6493e0ac5ece15929613', 'Authorization');
       return this;
     },
-              
     _get: function(path, params, succ, fail) {
         var url = this.dribbbleHost + path
         this.requestManager.GET_parameters_success_failure(url, params,
@@ -28,7 +28,6 @@ defineJSClass('FFDataSource', {
             })
         );
     },
-
     loadPublicShots: function(page, per_page, succ, fail) {
         var path = '/shots';
         this._get(path, {page: page, per_page:per_page}, succ, fail)
@@ -42,7 +41,6 @@ defineJSClass('FFDataSource', {
         var path = '/users/' + userId + '/shots';
         this._get(path, {page: page, per_page:per_page}, succ, fail)
     },
-              
 },
 {
     shareInstance: function() {
